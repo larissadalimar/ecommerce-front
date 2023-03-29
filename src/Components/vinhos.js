b88f6fb6dbf05557d675caaf6b0d279766667eb7
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-29 14:53:08 -0300

    feat

--- src/Components/vinhos.js
+++ src/Components/vinhos.js
@@ -97,6 +97,194 @@ export const wines = [
     },
 ]
 
+POSTEGRES
+
+    (
+        'https://www.wine.com.br/cdn-cgi/image/q=99,f=png,h=176/assets-images/produtos/20884-01.png',
+        'Davideira',
+        'O nome Davideira faz alusão ao fruto que sai da videira, a uva. Em Portugal, as uvas crescem há mais de 4 mil anos. É um cultivo que faz parte da história do país. Esse vinho é uma homenagem à uva, elaborado com castas típicas do país.',
+        'Portugal Tinto Meio Seco 750 ml',
+        '13.00% ABV',
+        6490,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/q=99,f=png,h=176/assets-images/produtos/27207-01.png',
+        'Pueblo del Sol',
+        'A uva que tornou-se símbolo do Uruguai e hoje é responsável por fazer parte dos maiores vinhos do país, a Tannat! Gera exemplares potentes, com taninos firmes, frutados e macios. O Pueblo del Sol Tannat traz a autenticidade da uva em solo uruguaio, uma experiência esplêndida!',
+        'Uruguai Tinto Seco 750 ml',
+        '12.00% ABV',
+        6490,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/24258-01.png',
+        'Amandla',
+        'Um exemplar que apresenta a Pinotage bem ao estilo sul-africano: leve, frutada e fácil de beber. Por ser proveniente de “bush vines”, vinhas arbustos, as uvas têm maior concentração de cores, aromas e sabores. Um exemplar que foge do estereótipo de uma Pinotage pesada e agressiva.',
+        'África do Sul Tinto Seco 750 ml',
+        '13.50% ABV',
+        10490,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26594-01.png',
+        'Ernst Loosen Private Reserve Riesling',
+        'Produzido por um dos maiores nomes da vitivinicultura alemã, Ernst Loosen, este branco tem um perfil muito aromático e saboroso. Elaborado em Mosel, esse rótulo traz a Riesling, uva branca que ocupa mais de 60% dos vinhedos da região.',
+        'Alemanha Branco Meio Seco 750 ml',
+        '12.00% ABV',
+        17990,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/24320-01.png',
+        'DiamAndes de Uco Cabernet Sauvignon',
+        'Um Cabernet Sauvignon argentino com essência francesa, assim é definido este exemplar. Um vinho que traz toda a potência do terroir argentino, com a elegância no estilo da França, auxiliada pela consultoria do renomado Michel Rolland, esta vinícola é do mesmo grupo que elabora o Clos de Los Siete. A DiamAndes traz qualidade e personalidade em seus exemplares.',
+        'Argentina Tinto Seco 750 ml',
+        '14.00% ABV',
+        29990,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28046-01.png',
+        'John Duval',
+        'John Duval Wines é uma renomada vinícola localizada no sul da Austrália que carrega mais de 100 anos de história. São mais de cinco gerações unindo conhecimentos tradicionais, avanços tecnológicos e respeito ao terroir na produção de vinhos de alta qualidade. Cada vinho é feito de uvas provenientes exclusivamente de vinhas velhas de Barossa, assinatura e estilo distinto da John Duval Wines, conhecido pela elegância, complexidade e estrutura. Esse exemplar é produzido com Grenache de safras excepcionais proveniente de dois vinhedos muito antigos e de baixo rendimento - 90% de um vinhedo ancestral do Vale do Éden com mais de 150 anos e 10% de vinhas de 55 anos plantadas nos ricos solos do Vale Barossa do Norte. Esse é um vinho tinto de pequena produção proveniente de vinhas velhas e de baixo rendimento, ou seja, frutas mais concentradas em nutrientes que geram vinhos com maior complexidade em aromas e sabores. Profundo e gostoso de beber, mostra frutas vermelhas de perfil mais fresco acompanhadas de notas florais, de ervas e de especiarias doces, que se confirmam na boca, tudo equilibrado por taninos aveludados e acidez vibrante, o que te faz almejar pelo próximo gole!',
+        'Austrália Tinto Meio Seco 750 ml',
+        '14.00% ABV',
+        98990,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28752-01.png',
+        'Ballade Cabernet Sauvignon',
+        'Ballade Cabernet Sauvignon é um vinho leve, frutado e harmônico da Miolo, vinícola brasileira reconhecida e premiada internacionalmente. Sinônimo de qualidade, tradição e pioneirismo no plantio de uvas finas no Brasil, tem na paixão pela vitivinicultura o desejo de levar mundo afora o vinho fino brasileiro, e isto tornou a Miolo a maior exportadora de vinhos do Brasil e a mais reconhecida no mercado internacional! A linha Ballade é produzida exclusivamente para a Wine, uma parceria de sucesso e muito bem avaliada que você só encontra aqui! Exemplar da variedade Cabernet Sauvignon cultivada em vinhedos próprios localizados na região da Campanha Central, Rio Grande do Sul. Vinho com perfil jovem, descomplicado, leve e muito agradável. Adequado para veganos.',
+        'Brasil Tinto Seco 750 ml',
+        '12.50% ABV',
+        4990,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/27590-01.png',
+        'Quinta Las Cabras Rosé',
+        'A linha de vinhos finos Quinta las Cabras é uma homenagem ao modo de vida que as primeiras famílias tiveram no passado em Las Cabras, região localizada em Peumo, na Denominação de Origem Valle de Cachapoal. As ''Quintas'', eram mansões familiares onde incontáveis histórias eram escritas, diferentes gerações viviam, apaixonadas pela terra e foi onde começaram a elaboração do vinho. Esse é um blend das principais uvas tintas da região, Cabernet Franc, Syrah, Malbec e Pinot Noir, com um toque da aromática Pinot Grigio, que também passa um tempo em contato com suas borras ganhando mais volume em boca e nuances perfumadas.',
+        'Chile Rosé Seco 750 ml',
+        '12.00% ABV',
+        11990,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28369-01.png',
+        'Montado Branco',
+        'Montado Branco é um blend das uvas Verdejo e Chardonnay de Toledo, uma antiga cidade localizada em uma colina sobre as planícies de Castilla-La Mancha, no centro da Espanha, que tem como objetivo capturar a essência da uva em seu vinho, proporcionando, assim, um exemplar que expresse as características naturais das uvas e do seu terroir. As uvas foram colhidas a noite para garantir o frescor, teve maceração à baixas temperaturas, o mosto passou por fermentação malolática, tudo para deixar o vinho com uma característica mais cremosa no paladar, além, claro, de manter nas notas frutadas e preservar o seu frescor!',
+        'Espanha Branco Seco 750 ml',
+        '12.50% ABV',
+        8990,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/26575-01.png',
+        'Apothic Red',
+        'Produzido no terroir californiano, esse tinto mescla as variedades Zinfandel, Syrah, Cabernet Sauvignon e Merlot. Apothic é um vinho inspirado nas antigas Apothecas (adegas subterrâneas), um lugar misterioso onde há mais de 800 anos os viticultores misturavam e armazenavam seus cobiçados vinhos.',
+        'Estados Unidos Tinto Meio Seco 750 ml',
+        '13.60% ABV',
+        20990,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/20202-01.png',
+        'Château La Fleur-Pétrus A.O.C. Pomerol',
+        'Uma das grandes estrelas de Pomerol, o Château La Fleur-Pétrus produz apenas um rótulo, com garrafas muito limitadas. Situado entre os renomados Château Lafleur e o Petrus, esse produtor conquista a cada safra incríveis pontuações dos críticos mais renomados do mundo do vinho.',
+        'França Tinto Seco 750 ml',
+        '13.50% ABV',
+        1094900,
+		'wine'
+    ),
+    (
+        'https://www.wine.com.br/cdn-cgi/image/f=png,h=238,q=99/assets-images/produtos/28056-01.png',
+        'I Giusti & Zanza Nemorino',
+        'I Giusti & Zanza Vigneti nasceu em 1996 da restauração de uma vinícola histórica nas colinas de Fauglia, em uma região da Toscana próxima ao mar Mediterrâneo, na área de Pisa, que tem um longo histórico de cultivo de vinho datado desde o início do século XVI. Os métodos de agricultura orgânica com uso de compostos orgânicos e biodinâmicos nas vinhas permaneceram como base da produção da vinícola que adota técnicas que respeitam a natureza, as características naturais das uvas e terroir e que busca uma harmonia na relação entre o ser humano e o lugar. O resultado é um vinho autêntico, expressivo e com muito frescor que conquista a cada gole, uma verdadeira viagem pela Toscana!',
+        'Itália Branco Seco 750 ml',
+        '13.00% ABV',
+        23990,
+		'wine'
+    )
+    ///// TAÇAS
+    (
+        'https://a-static.mlcdn.com.br/280x210/jogo-de-6-tacas-340ml-para-agua-e-vinho-boston-casambiente/medeirospresentes/15978824871/0489f94bde9e963e3c1265a919fa3dc4.jpeg',
+        'Taça 340ml',
+        'Taça de vidro para Vinho Tinto',
+        'Brazil',
+        '',
+        '5579',
+        'cup',
+        ''
+    ),
+    (
+        'https://a-static.mlcdn.com.br/280x210/jogo-de-taca-de-cristal-para-vinho-tinto-com-6-unidades-580ml-bohemia/gahe/4531895819/8468a159a13e155cfaf42e7092d6161d.jpeg',
+        'Taça de Cristal 580ml',
+        'Taça de Cristal para vinho Tinto',
+        'Brazil',
+        '',
+        10733,
+        'cup',
+        ''
+    ),
+    (
+        'https://a-static.mlcdn.com.br/280x210/jogo-de-taca-de-cristal-para-vinho-bordeau-com-6-unidades-gastro-bohemia-650ml/gahe/4531895807/049bf7c5670959303083ebd90add49f5.jpeg',
+        'Taça de Cristal 650ml',
+        'Taça de vinho Bordeau Gastro Bohemia',
+        'Brazil',
+        '',
+        12068,
+        'cup',
+        ''
+    ),
+    (
+        'https://a-static.mlcdn.com.br/280x210/jogo-6-tacas-para-agua-e-vinho-em-cristal-560ml-xtra-bohemia/medeirospresentes/15864148520/fa8419ce89df731230469b201048cf6f.jpeg',
+        'Taça de Cristal 560ml',
+        'Taça para água e vinho em Cristal Ctra Bohemia',
+        'Brazil',
+        '',
+        18599,
+        'cup',
+        ''
+    )
+    ///// tabuas
+    (
+        'https://a-static.mlcdn.com.br/280x210/tabua-para-queijos-multiuso-frios-petiscos-brotinho-em-bambu-clink/limixdistribuidora/ck3572-3/866950bb87403d8568fee061971937c1.jpeg',
+        'Tábua para queijos',
+        'Essa tabua é perfeita para diversos tipos de queijos, feita em bambu com otima qualidade',
+        'Brazil',
+        '',
+        4588,
+        'taboo'
+    ),
+    (
+       'https://a-static.mlcdn.com.br/280x210/tabua-de-servir-queijos-aperitivos-frios-em-madeira-pinus-carmisini/carmisinidesign/15939173264/0e46f1eb8cd35149cb3299e548843fc7.jpeg',
+       'Tábua Coração',
+       'Essa tábua em formato de coração é muito aconchegante e perfeita para servir aperitivos frios, feita com mandeira de Pinus - Carmisini',
+       'Brazil',
+       '',
+       4499,
+       'taboo' 
+    ),
+    (
+        'https://a-static.mlcdn.com.br/280x210/tabua-de-queijos-vinho-e-taca-petisqueira-redonda-madeira-full-vendas/olistplus/opmx3qaovmm0owc1/4f0ce37d7ebcd7275d4d815161d9585b.jpeg',
+        'Tábua redonda',
+        'Essa tábua de queijos é otima para servir variados petiscos e seu formato redondo é muito acolhedora e romantica, possue até suporte para colocar taça',
+        'Brazil',
+        '',
+        7970,
+        'taboo'
+    ),
+    (
+        'https://a-static.mlcdn.com.br/280x210/tabua-de-queijo-e-frios-bambu-jogo-kit-com-4-talheres-inox-2-potes-2-mini-lousa-2-giz-e-gaveta-churrasco-alpha/churrascoalpha/120/3eab05e061fbc451aab6fcca1077c4f1.jpeg',
+        'Tábua Gaveteiro',
+        'Essa tábua é otima para quem gosta de versalidade, com um otimo espaço, vem com 4 talheres inox, 2 potes, 2 mini lousa, 2 giz, feita com bambu',
+        'Brazil',
+        '',
+        32900,
+        'taboo'
+    )
 // {
 //     image:,
 //     name:,
@@ -104,4 +292,5 @@ export const wines = [
 //     type:,
 //     alcohol:,
 //     value:
+//     type_product:
 // },
